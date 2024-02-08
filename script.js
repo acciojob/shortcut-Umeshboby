c0620c64af469c99b2481a2493240f38dd00a99e
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2024-02-08 12:07:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function shortcut(s1, s2) {
 	return initial;
 
 }
-// }
+}
 
-// // Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(shortcut(s1, s2));
+// Do not change the code below.
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(shortcut(s1, s2));
